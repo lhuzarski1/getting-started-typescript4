48bd236a31e028c15faf540b00afd5662c2ffca6
lhuzarski1 <lukaszhuzarski1@gmail.com>
2022-11-25 15:29:29 +0100

    creating and using custom types

--- app/app.ts
+++ app/app.ts
@@ -1,50 +1,19 @@
-function startGame() {
+/// <reference path="player.ts" />
+/// <reference path="game.ts" />
 
-  let playerName: string | undefined = getInputValue("playername");
-  logPlayer(playerName);
+let newGame: Game;
 
-  postScore(80, playerName);
-  postScore(-5, playerName);
-}
+document.getElementById('startGame')!.addEventListener('click', () => {
+  const player: Player = new Player();
+  player.name = Utility.getInputValue('playername');
 
-function logPlayer(name: string = "Multimath Player"): void {
-  console.log(`New game starting for player: ${name}`);
-}
+  const problemCount: number = Number(Utility.getInputValue('problemCount'));
+  const factor: number = Number(Utility.getInputValue('factor'));
 
-function getInputValue(elementID: string): string | undefined {
-  const inputElement: HTMLInputElement = <HTMLInputElement>(
-    document.getElementById(elementID)
-  );
+  newGame = new Game(player, problemCount, factor);
+  newGame.displayGame();
+});
 
-  if (inputElement.value === "") {
-    return undefined;
-  } else {
-    return inputElement.value;
-  }
-}
-
-function postScore(
-  score: number,
-  playerName: string = "Multimath Player"
-): void {
-  let logger: (value: string) => void;
-
-  if (score < 0) {
-    logger = logError;
-  } else {
-    logger = logMessage;
-  }
-
-  const scoreElement: HTMLElement | null =
-    document.getElementById("postedScores");
-  scoreElement!.innerText = `${score} - ${playerName}`;
-
-  logger(`Score: ${score}`);
-}
-
-document.getElementById("startGame")!.addEventListener("click", startGame);
-
-const logMessage = (message: string) => console.log(message);
-function logError(err: string): void {
-  console.error(err);
-}
+document.getElementById('calculate')!.addEventListener('click', () => {
+  newGame.calculateScore();
+});
